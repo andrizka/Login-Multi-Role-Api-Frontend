46d49d0a2de30d8e6c834fc54ed74485c9f36a7a
andrizka <andrianramadhan99@gmail.com>
2024-02-26 00:23:14 +0700

    first commit

--- src/components/FormAddProduct.jsx
+++ src/components/FormAddProduct.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export const FormAddProduct = () => {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [msg, setMsg] = useState('');
+  const navigate = useNavigate();
+
+  const saveProduct = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:5000/products', {
+        name: name,
+        price: price,
+      });
+      navigate('/products');
+    } catch (error) {
+      if (error.response) {
+        setMsg(error.response.data.msg);
+      }
+    }
+  };
   return (
     <div>
       <h1 className="title">Products</h1>
@@ -8,23 +29,26 @@ export const FormAddProduct = () => {
       <div className="card is-shadowless">
         <div className="card-content">
           <div className="content">
-            <form className="">
+            <form onSubmit={saveProduct}>
+              <p className="has-text-centered">{msg}</p>
               <div className="field">
                 <label className="label">Product Name</label>
                 <div className="control">
-                  <input type="text" className="input" placeholder="Product Name" />
+                  <input type="text" className="input" value={name} onChange={(e) => setName(e.target.value)} placeholder="Product Name" />
                 </div>
               </div>
               <div className="field">
                 <label className="label">Price</label>
                 <div className="control">
-                  <input type="text" className="input" placeholder="Price" />
+                  <input type="text" className="input" value={price} onChange={(e) => setPrice(e.target.value)} placeholder="Price" />
                 </div>
               </div>
 
               <div className="field ">
                 <div className="control">
-                  <button className="button is-success">Save</button>
+                  <button type="submit" className="button is-success">
+                    Save
+                  </button>
                 </div>
               </div>
             </form>
